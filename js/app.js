d40426ac75c573f2f8a8ba73848cddfd3cd4581a
Ben Howdle <benhowdle89@gmail.com>
2012-09-16 23:05:57 +0100

    js scaffolding

--- js/app.js
+++ js/app.js
@@ -2,15 +2,54 @@ var costarred = {
 	apiKey: '23afca60ebf72f8d88cdcae2c4f31866',
 	baseUrl: 'http://api.themoviedb.org/3/',
 	matches: [],
-	init: function(){},
+	results: $('#results'),
+	searchButton: $('.controls button'),
+	input1: $('#actor1'),
+	input2: $('#actor2'),
+	init: function(){
+		searchButton.click(function(){
+			this.search([input1.val(), input2.val()]);
+		});
+	},
 	buildUrl: function(type, query){
 		return this.baseUrl + '/' + type + '?api_key=' + this.apiKey + '&query=' + query,
 	},
-	ajax: function(){},
+	ajax: function(url, data, callback){
+		$.ajax({
+		  type: 'POST',
+		  url: url,
+		  data: {},
+		  success: function(json){
+		  	costarred[callback](json);
+		  },
+		  dataType: 'json'
+		});
+	},
 	findMatches: function(){},
 	toggleResults: function(){},
 	toggleInputs: function(){},
-	findName: function(){},
+	clearInputs: function(){},
+	findName: function(json){
+		return ;
+	},
 	findPoster: function(){},
-	findThumb: function(){}
+	findThumb: function(){},
+	doSearch: function(term){
+		this.ajax('people', );
+	},
+	clearMovies: function(){
+		this.matches = [];
+	},
+	search: function(terms){
+		var termsL = terms.length;
+		while(termsL--){
+			matches.push(costarred.doSearch(terms[termsL]));
+		}
+	}
 };
+
+$(document).ready(function(){
+
+	costarred.init();
+
+});
