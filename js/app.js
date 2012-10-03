d6f1ad4472b0ca0dd0517c9d4cca1433590c2963
Ben Howdle <benhowdle89@gmail.com>
2012-10-04 00:36:44 +0100

    so many things, js structure, ajax, handlebars

--- js/app.js
+++ js/app.js
@@ -3,141 +3,113 @@ jQuery(function( $ ) {
 	'use strict';
 
 	var Utils = {
-	
+		ajax: function(url, success, context){
+			$.ajax({
+			  type: "POST",
+			  url: url,
+			  dataType: 'jsonp'
+			}).done(function(msg){
+				success.call(context, msg);	
+			});
+		}
 	};
 
 	var App = {
 		init: function() {
-			apiKey: '23afca60ebf72f8d88cdcae2c4f31866',
-			baseUrl: 'http://api.themoviedb.org/3/'
-			this.todos = Utils.store('todos-jquery');
+			this.apiKey = '23afca60ebf72f8d88cdcae2c4f31866';
+			this.baseUrl = 'http://api.themoviedb.org/3/';
+			this.films = [];
+			this.actors =  [];
+			this.titles = [];
+			this.doRender - [];
+			this.classTranslate = ['first', 'second'];
 			this.cacheElements();
 			this.bindEvents();
-			this.render();
+			this.setupUi();
+		},
+		buildPersonSearchUrl: function(person){
+			return this.baseUrl + 'search/person?query=' + person + '&api_key=' + this.apiKey; 
+		},
+		buildMovieListUrl: function(id){
+			return this.baseUrl + 'person/' + id + '/credits?api_key=' + this.apiKey; 
+		},
+		storeActor: function(json){
+			var that = this;
+			var id = json.results[0].id;
+			var name = json.results[0].name;
+			var i = (this.actors.length == 1) ? 1 : 0;
+			var data = {
+				id: id,
+				name: name,
+				actorClass: this.classTranslate[i]
+			};
+			this.actors.push(data);
+			Utils.ajax(that.buildMovieListUrl(id), that.storeFilms, that);
+		},
+		storeFilms: function(json){
+			var x = 0;
+			for(x in json.cast){
+				var title = json.cast[x].original_title;
+				this.films.push({title: title});
+			}
+			this.doRender.push(true);
+			if(this.doRender.length > 1){
+				this.getMatches(this.films);
+				this.renderResults();
+			}
+		},
+		getMatches: function(movies) {
+		  var ref;
+		  for(var i in movies) {
+		    ref = movies[i].title;
+		    if(this.titles.indexOf(ref) == -1) {
+		    	movies.splice(i, 1);
+		    } else {
+		    	this.titles.push(ref);
+		    }
+		  }
+		  this.films = movies;
 		},
 		cacheElements: function() {
-			this.todoTemplate = Handlebars.compile( $('#todo-template').html() );
-			this.footerTemplate = Handlebars.compile( $('#footer-template').html() );
-			this.$todoApp = $('#todoapp');
-			this.$newTodo = $('#new-todo');
-			this.$toggleAll = $('#toggle-all');
-			this.$main = $('#main');
-			this.$todoList = $('#todo-list');
-			this.$footer = this.$todoApp.find('#footer');
-			this.$count = $('#todo-count');
-			this.$clearBtn = $('#clear-completed');
+			this.$films = $('#films');
+			this.$actors = $('.actors');
+			this.$controls = $('.controls');
+			this.$noResults = $('.no-results');
+			this.$yesResults = $('.yes-results');
+			this.$inputs = $('.inputs input[type="text"]');
+			this.filmTemplate = Handlebars.compile( $('#film-template').html() );
+			this.actorTemplate = Handlebars.compile( $('#actor-template').html() );
 		},
 		bindEvents: function() {
-			var list = this.$todoList;
-			this.$newTodo.on( 'keyup', this.create );
-			this.$toggleAll.on( 'change', this.toggleAll );
-			this.$footer.on( 'click', '#clear-completed', this.destroyCompleted );
-			list.on( 'change', '.toggle', this.toggle );
-			list.on( 'dblclick', 'label', this.edit );
-			list.on( 'keypress', '.edit', this.blurOnEnter );
-			list.on( 'blur', '.edit', this.update );
-			list.on( 'click', '.destroy', this.destroy );
-		},
-		render: function() {
-			this.$todoList.html( this.todoTemplate( this.todos ) );
-			this.$main.toggle( !!this.todos.length );
-			this.$toggleAll.prop( 'checked', !this.activeTodoCount() );
-			this.renderFooter();
-			Utils.store( 'todos-jquery', this.todos );
-		},
-		renderFooter: function() {
-			var todoCount = this.todos.length,
-				activeTodoCount = this.activeTodoCount(),
-				footer = {
-					activeTodoCount: activeTodoCount,
-					activeTodoWord: Utils.pluralize( activeTodoCount, 'item' ),
-					completedTodos: todoCount - activeTodoCount
-				};
-
-			this.$footer.toggle( !!todoCount );
-			this.$footer.html( this.footerTemplate( footer ) );
-		},
-		toggleAll: function() {
-			var isChecked = $( this ).prop('checked');
-			$.each( App.todos, function( i, val ) {
-				val.completed = isChecked;
-			});
-			App.render();
-		},
-		activeTodoCount: function() {
-			var count = 0;
-			$.each( this.todos, function( i, val ) {
-				if ( !val.completed ) {
-					count++;
-				}
-			});
-			return count;
-		},
-		destroyCompleted: function() {
-			var todos = App.todos,
-				l = todos.length;
-			while ( l-- ) {
-				if ( todos[l].completed ) {
-					todos.splice( l, 1 );
-				}
-			}
-			App.render();
-		},
-		// Accepts an element from inside the ".item" div and
-		// returns the corresponding todo in the todos array
-		getTodo: function( elem, callback ) {
-			var id = $( elem ).closest('li').data('id');
-			$.each( this.todos, function( i, val ) {
-				if ( val.id === id ) {
-					callback.apply( App, arguments );
-					return false;
-				}
+			var controls = this.$controls;
+			var that = this;
+			controls.on( 'click', 'button', function(){
+				that.search();
 			});
 		},
-		create: function(e) {
-			var $input = $(this),
-				val = $.trim( $input.val() );
-			if ( e.which !== App.ENTER_KEY || !val ) {
-				return;
-			}
-			App.todos.push({
-				id: Utils.uuid(),
-				title: val,
-				completed: false
-			});
-			$input.val('');
-			App.render();
-		},
-		toggle: function() {
-			App.getTodo( this, function( i, val ) {
-				val.completed = !val.completed;
-			});
-			App.render();
-		},
-		edit: function() {
-			$(this).closest('li').addClass('editing').find('.edit').focus();
-		},
-		blurOnEnter: function( e ) {
-			if ( e.keyCode === App.ENTER_KEY ) {
-				e.target.blur();
-			}
-		},
-		update: function() {
-			var val = $.trim( $(this).removeClass('editing').val() );
-			App.getTodo( this, function( i ) {
-				if ( val ) {
-					this.todos[ i ].title = val;
-				} else {
-					this.todos.splice( i, 1 );
-				}
-				this.render();
+		search: function(){
+			var that = this;
+			that.actors = [];
+			that.films = [];
+			that.titles = [];
+			this.doRender = [];
+			this.$inputs.each(function(){
+				if($(this).val() == '') return;
+				Utils.ajax(that.buildPersonSearchUrl($(this).val()), that.storeActor, that);
 			});
 		},
-		destroy: function() {
-			App.getTodo( this, function( i ) {
-				this.todos.splice( i, 1 );
-				this.render();
-			});
+		setupUi: function(){
+			this.$noResults.hide();
+		},
+		renderResults: function(){
+			this.$films.html( this.filmTemplate( this.films ) );
+			this.$actors.html( this.actorTemplate( this.actors ) );
+			this.$noResults.hide();
+			this.$yesResults.show();
+		},
+		renderNoResults: function(){
+			this.$noResults.show();
+			this.$yesResults.hide();
 		}
 	};
 
