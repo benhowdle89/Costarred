731e40a7d9f993be9ce6846c0e6938d16a17754f
Ben Howdle <benhowdle89@gmail.com>
2012-10-04 21:04:29 +0100

    errors and films

--- js/app.js
+++ js/app.js
@@ -18,6 +18,7 @@ jQuery(function( $ ) {
 		init: function() {
 			this.apiKey = '23afca60ebf72f8d88cdcae2c4f31866';
 			this.baseUrl = 'http://api.themoviedb.org/3/';
+			this.imageUrl = 'http://cf2.imgobject.com/t/p/w185/';
 			this.films = [];
 			this.actors =  [];
 			this.titles = [];
@@ -37,11 +38,13 @@ jQuery(function( $ ) {
 			var that = this;
 			var id = json.results[0].id;
 			var name = json.results[0].name;
+			var profile = that.imageUrl + json.results[0].profile_path;
 			var i = (this.actors.length == 1) ? 1 : 0;
 			var data = {
 				id: id,
 				name: name,
-				actorClass: this.classTranslate[i]
+				actorClass: this.classTranslate[i],
+				profile: profile
 			};
 			this.actors.push(data);
 			Utils.ajax(that.buildMovieListUrl(id), that.storeFilms, that);
@@ -50,25 +53,35 @@ jQuery(function( $ ) {
 			var x = 0;
 			for(x in json.cast){
 				var title = json.cast[x].original_title;
-				this.films.push({title: title});
+				var poster = this.imageUrl + json.cast[x].poster_path;
+				this.films.push({title: title, poster: poster});
 			}
 			this.doRender.push(true);
 			if(this.doRender.length > 1){
 				this.getMatches(this.films);
-				this.renderResults();
+				if(this.films.length){
+					this.renderResults();	
+				} else {
+					this.populateErrors();
+					this.renderNoResults();
+				}
+				
 			}
 		},
+		populateErrors: function(){
+			this.$errorFirstActor.text(this.actors[0].name);
+			this.$errorSecondActor.text(this.actors[1].name);
+		},
 		getMatches: function(movies) {
-		  var ref;
-		  for(var i in movies) {
-		    ref = movies[i].title;
-		    if(this.titles.indexOf(ref) == -1) {
-		    	movies.splice(i, 1);
-		    } else {
-		    	this.titles.push(ref);
-		    }
-		  }
-		  this.films = movies;
+			var temp = {}, newArr = [];
+			for(var k =0;k< movies.length; k++){
+			    if(temp[movies[k].title]){
+			        newArr.push(movies[k]);
+			    }
+			    
+			temp[movies[k].title] = true;
+			}
+			this.films = newArr;
 		},
 		cacheElements: function() {
 			this.$films = $('#films');
@@ -76,6 +89,8 @@ jQuery(function( $ ) {
 			this.$controls = $('.controls');
 			this.$noResults = $('.no-results');
 			this.$yesResults = $('.yes-results');
+			this.$errorFirstActor = $('#firstActor', this.$noResults);
+			this.$errorSecondActor = $('#secondActor', this.$noResults);
 			this.$inputs = $('.inputs input[type="text"]');
 			this.filmTemplate = Handlebars.compile( $('#film-template').html() );
 			this.actorTemplate = Handlebars.compile( $('#actor-template').html() );
